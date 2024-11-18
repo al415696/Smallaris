7b398f89017a87e7575919043a0bb6ed0e9eb0f7
al415696 <95293214+al415696@users.noreply.github.com>
2024-11-18 20:38:27 +0100

    merge(core): combinado r3-hu1 con it-01 y resuelto conflictos

--- app/src/main/java/es/uji/smallaris/model/RepositorioFirebase.kt
+++ app/src/main/java/es/uji/smallaris/model/RepositorioFirebase.kt
@@ -1,6 +1,6 @@
 package es.uji.smallaris.model
 
-class RepositorioFirebase : RepositorioVehiculos{
+class RepositorioFirebase : RepositorioVehiculos, RepositorioLugares{
     override fun getVehiculos(): List<Vehiculo> {
         TODO("Not yet implemented")
     }
@@ -11,5 +11,13 @@ class RepositorioFirebase : RepositorioVehiculos{
 
     override fun updateVehiculos(viejo: Vehiculo, nuevo: Vehiculo): Boolean {
         TODO("Not yet implemented")
+    }
+
+    override fun getLugares(): List<LugarInteres> {
+        return mutableListOf()
+    }
+
+    override fun addLugar(lugar: LugarInteres): Boolean {
+        return true
     }
 }
