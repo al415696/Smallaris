3c4cfc49ae0a943edb8013d24d9eb3dc31306fbe
MCY-1911 <al415647@uji.es>
2024-11-18 11:48:22 +0100

    test(it01|r2-hu01): crear tests de aceptación addLugar
    Se incluyen cuatro tests de aceptación:
    - Coords válidas no dadas de alta previamente
    - Coords válidas pero dadas de alta previamente
    - Latitud inválida
    - Longitud inválida

---
+++ app/src/main/java/es/uji/smallaris/model/ServicioAPIs.kt
@@ -0,0 +1,12 @@
+package es.uji.smallaris.model
+
+class ServicioAPIs {
+
+    public fun getToponimoCercano(longitud: Float, latitud: Float): String {
+        return ""
+    }
+
+    public fun apiEnFuncionamiento(servicio: API): Boolean {
+        return true
+    }
+}
