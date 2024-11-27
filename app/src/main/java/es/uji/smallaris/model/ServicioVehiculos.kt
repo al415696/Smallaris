4fe54730d5e3962479bebd8bbdc855dfb65c5ea5
al415696 <95293214+al415696@users.noreply.github.com>
2024-11-27 10:27:17 +0100

    feat(it01h01): Añadido la implementación en servicioVehiculo para añadir
    - Añadido metodos para revisar la validez y si el vehiculo ya existia
    -Añadido nueva excepción de campo vacío para posible uso posterior
    -Añadido lógica para método añadir vehiculos de ServicioVehiculos

--- app/src/main/java/es/uji/smallaris/model/ServicioVehiculos.kt
+++ app/src/main/java/es/uji/smallaris/model/ServicioVehiculos.kt
@@ -10,13 +10,32 @@ class ServicioVehiculos(private val repositorio: RepositorioVehiculos) {
 
 
     fun addVehiculo (nombre: String, consumo: Double, matricula: String, tipo: TipoVehiculo): Vehiculo? {
+        var vehiculo: Vehiculo
+        //        Checks de validez de datos tienen que estar aquí no en las clases que use
+        if (checkValidezVehiculo(nombre, consumo, matricula, tipo)){
+            vehiculo = Vehiculo(nombre = nombre, consumo = consumo, matricula = matricula, tipo = tipo)
+            if (!checkUnicidadVehiculo(nombre, matricula))
+                throw VehicleAlredyExistsException()
+            //        Se ejecuta el método add del repositorio
 
-
-//        Checks de validez de datos tienen que estar aquí no en las clases que use
-        var vehiculo = Vehiculo(nombre = nombre, consumo = consumo, matricula = matricula, tipo = tipo)
-//        se ejecuta el método add del repositorio
-        repositorio.addVehiculos(vehiculo)
+            repositorio.addVehiculos(vehiculo)
+            return vehiculo
+        }
         return null
+    }
+    private fun checkValidezVehiculo(nombre: String, consumo: Double, matricula: String, tipo: TipoVehiculo): Boolean{
+        return nombre.isNotEmpty() && matricula.isNotEmpty() && consumo >=0
+    }
+    private fun checkUnicidadVehiculo(nombre: String,  matricula: String): Boolean{
+        for (otro in vehiculos){
+            if (nombre == otro.nombre){
+                return false
+            }
+            if (matricula == otro.matricula){
+                return false
+            }
+        }
+        return true
     }
 
     fun getVehiculos(): List<Vehiculo>{
