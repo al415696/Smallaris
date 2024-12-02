411eb354d6ac8eccae8440d063b5067df1efafac
al415696 <95293214+al415696@users.noreply.github.com>
2024-12-03 00:08:03 +0100

    feat(r5-hu3): Añadido métodos para asignar lugares como favoritos

--- app/src/main/java/es/uji/smallaris/model/RepositorioLugares.kt
+++ app/src/main/java/es/uji/smallaris/model/RepositorioLugares.kt
@@ -3,4 +3,5 @@ package es.uji.smallaris.model
 interface RepositorioLugares: Repositorio {
     suspend fun getLugares(): List<LugarInteres>
     suspend fun addLugar(lugar: LugarInteres): Boolean
+    suspend fun setLugarInteresFavorito(lugar: LugarInteres, favorito: Boolean = true): Boolean
 }
