b1456e5d03f7e9a3721d8ffb74821dc6a92761d4
al415696 <95293214+al415696@users.noreply.github.com>
2024-12-11 18:05:39 +0100

    feat(r5-hu05): Añadido funcionalidad para pasar la nuevas pruebas

--- app/src/main/java/es/uji/smallaris/model/RepositorioRutas.kt
+++ app/src/main/java/es/uji/smallaris/model/RepositorioRutas.kt
@@ -3,4 +3,5 @@ package es.uji.smallaris.model
 interface RepositorioRutas: Repositorio {
     suspend fun getRutas(): List<Ruta>
     suspend fun addRuta(ruta: Ruta): Boolean
+    suspend fun setRutaFavorita(ruta: Ruta, favorito:Boolean = true): Boolean
 }
