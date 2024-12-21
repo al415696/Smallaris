4eb3a4c81b7be48afd139b3683a39e2974ef8fd8
MCY-1911 <al415647@uji.es>
2024-12-21 17:14:30 +0100

    chore(it-04|r2-gui): Mapa en composable de mapa

--- app/src/main/java/es/uji/smallaris/ui/screens/MapaScreen.kt
+++ app/src/main/java/es/uji/smallaris/ui/screens/MapaScreen.kt
@@ -1,15 +1,28 @@
 package es.uji.smallaris.ui.screens
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.mapbox.geojson.Point
+import com.mapbox.maps.extension.compose.MapboxMap
+import com.mapbox.maps.extension.compose.animation.viewport.rememberMapViewportState
 import es.uji.smallaris.ui.state.MapaViewModel
 
 @Composable
 fun MapaScreen(
     viewModel: MapaViewModel
 ){
-    Surface {
-        Text("se supone que es Mapa")
-    }
+    MapboxMap(
+        Modifier.fillMaxSize(),
+        mapViewportState = rememberMapViewportState {
+            setCameraOptions {
+                zoom(15.0) // Ajusta el nivel de zoom según lo que desees mostrar.
+                center(Point.fromLngLat(-0.068547, 39.994259)) // Coordenadas de la Universidad Jaume I.
+                pitch(0.0)
+                bearing(0.0)
+            }
+        },
+    )
 }
