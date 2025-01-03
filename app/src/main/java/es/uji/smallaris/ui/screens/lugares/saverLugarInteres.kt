07275eeb194329e6472533c95c6a60e24efd377e
MCY-1911 <97406896+MCY-1911@users.noreply.github.com>
2025-01-03 20:45:31 +0100

    fix(it04|r5): Persistencia de favoritos (#115) (#116)

--- app/src/main/java/es/uji/smallaris/ui/screens/lugares/saverLugarInteres.kt
+++ app/src/main/java/es/uji/smallaris/ui/screens/lugares/saverLugarInteres.kt
@@ -5,13 +5,15 @@ import androidx.compose.runtime.saveable.listSaver
 import es.uji.smallaris.model.lugares.LugarInteres
 
 val saverLugarInteres: Saver<LugarInteres, *> = listSaver(
-    save = { listOf(it.longitud, it.latitud, it.nombre, it.municipio) },
+    save = { listOf(it.longitud, it.latitud, it.nombre, it.municipio, it.isFavorito()) },
     restore = {
-        LugarInteres(
+        val lugar = LugarInteres(
             longitud = it[0] as Double,
             latitud = it[1] as Double,
             nombre = it[2] as String,
-            municipio = it[3] as String
+            municipio = it[3] as String,
         )
+        lugar.setFavorito(it[4] as Boolean)
+        lugar
     }
 )
